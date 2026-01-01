be041a65ec724ed709d8299c22d77eec3b9da878
Aurelien Benel <aurelien.benel@utt.fr>
2026-01-01 17:35:25 +0100

    FEATURE: Consult course contribution by skill block.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import data from './assets/sample_data.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1> TeknUTopia </h1>
+      </header>
+      <main className="container">
+        { Object.entries(
+            Object.groupBy(data, ({skill}) => skill)
+          ).map(([skill, contributions], i) =>
+            <SkillBlock {...{skill, contributions}} key={i} />
+          )
+        }
+      </main>    
     </>
+  )
+}
+
+function SkillBlock({skill, contributions}) {
+  return (
+    <div>
+      <header> {skill} </header>
+      { contributions.map((y, j) =>
+        <Contribution {...y} key={j} />
+      )}
+    </div>
+  );
+}
+
+function Contribution({course}) {
+  return (
+    <article>
+      {course}
+    </article>
   )
 }
 
