b6a2ae1dc056d85ff788b7ad9a7ea0f61f06f51f
Aurelien Benel <aurelien.benel@utt.fr>
2026-01-02 11:28:04 +0100

    FEATURE: Select courses to be part of one's own curriculum.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { generateRandomLightColor } from 'make-random-color'
+import { useState } from 'react'
 import data from './assets/sample_data.json'
 import './App.css'
 
 const contributionsData = data.filter(({skill, course}) => skill && course)
 
 const skillBlocksData = data.filter(({skill, title}) => skill && title)
+
+const courseData = data.filter(({course, title}) => course && title)
 
 const colors = contributionsData.reduce(
   (result, {course}) => result[course]
@@ -14,6 +17,16 @@ const colors = contributionsData.reduce(
 )
 
 function App() {
+  const [selectedCourses, setSelectedCourses] = useState([])  
+
+  function toggleCourse(id) {
+    if (selectedCourses.includes(id)) {
+      setSelectedCourses(selectedCourses.filter(x => x !== id))
+    } else {
+      setSelectedCourses([...selectedCourses, id])    
+    }
+  }
+
   return (
     <>
       <header>
@@ -23,15 +36,24 @@ function App() {
         { Object.entries(
             Object.groupBy(contributionsData, ({skill}) => skill)
           ).map(([skill, contributions], i) =>
-            <SkillBlock {...{skill, contributions}} key={i} />
+            <SkillBlock {...{skill, contributions, selectedCourses}} key={i} />
           )
         }
+        <fieldset>
+          <legend>
+            <h2> Parcours à la carte </h2>
+          </legend>
+          { courseData.map(({course, title}) =>
+              <Course {...{course, title, toggleCourse}} key={course} />
+            )
+          }
+        </fieldset>
       </main>    
     </>
   )
 }
 
-function SkillBlock({skill, contributions}) {
+function SkillBlock({skill, contributions, selectedCourses}) {
   const {title} = skillBlocksData.find(x => x.skill.toString() === skill.toString());
   return (
     <article>
@@ -39,17 +61,29 @@ function SkillBlock({skill, contributions}) {
         <h3> {title} </h3>
       </header>
       { contributions.map((y, j) =>
-        <Contribution {...y} key={j} />
+        <Contribution {...y} {...{selectedCourses}} key={j} />
       )}
     </article>
   );
 }
 
-function Contribution({course}) {
+function Contribution({course, selectedCourses}) {
+  const display = selectedCourses.includes(course) ? 'block' : 'none';
   return (
-    <article style={{background: colors[course]}} >
+    <article style={{background: colors[course], display}} >
       {course}
     </article>
+  )
+}
+
+function Course({course, title, toggleCourse}) {
+  return (
+    <label>
+      <input type="checkbox" name={course}
+        onChange={() => toggleCourse(course)}
+      />
+        <b> {course} </b> — {title}
+    </label>
   )
 }
 
