benel/TeknUTopia

commit fc9d009c3a340cce8353ee631f08c27b19826ef8

PROCESS: Static Web application deployed on GitHub Pages.

vite.config.js
@@ -10,4 +10,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/TeknUTopia/',
 })

.github/workflows/deploy.yml
@@ -1,0 +1,44 @@
+name: Deploy static content to Pages
+
+on:
+  push:
+    branches: ["main"]
+  workflow_dispatch: # Manual trigger
+
+permissions:
+  contents: read
+  pages: write
+  id-token: write
+
+concurrency:
+  group: "pages"
+  cancel-in-progress: false
+
+jobs:
+  deploy:
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v5
+      - name: Set up NodeJS
+        uses: actions/setup-node@v6
+        with:
+          node-version: 24
+          cache: npm
+          cache-dependency-path: package-lock.json
+      - name: Install frontend dependencies
+        run: npm install
+      - name: Build frontend
+        run: npm run build
+      - name: Setup Pages
+        uses: actions/configure-pages@v5
+      - name: Upload artifact
+        uses: actions/upload-pages-artifact@v4
+        with:
+          path: dist
+      - name: Deploy to GitHub Pages
+        id: deployment
+        uses: actions/deploy-pages@v4

commit 6843a00fb6136c78f1ddf24fda1fd1daebf3179f

PROCESS: Development mode restored.
Regression caused by fc9d009.

vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/TeknUTopia/',
+  base: (process.env.NODE_ENV === 'production') ? '/TeknUTopia/' : '/',
 })